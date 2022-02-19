8ac84afaaf2955c649866b41d1a6fd9cf49ae362
dimchee <dimchee90@gmail.com>
2022-02-19 23:18:13 +0100

    Ready for beta :D

--- src/Main.elm
+++ src/Main.elm
@@ -8,22 +8,33 @@ import Browser.Events exposing (onKeyPress)
 import Json.Decode as Decode
 import Html exposing (button)
 import Html.Events exposing (onClick)
+import List exposing (foldl, foldr)
+import Process
+import Task
+import Words
+import Random
+import Array
 
-
+-- TODO make virtual keyboard?
+-- TODO animations like this? https://dev.to/lucamug/elm-beginners-tutorial-how-to-make-animated-snackbars-with-zero-css-12g1
+-- TODO write tests
+-- using this for colors https://www.tedmontgomery.com/tutorial/clrctgBL.html
 type Word = Word (List Char)
+
 -- Use this for word https://github.com/Orasund/elm-static-array
-type alias Model =
+type alias State =  
     { gueses : List Word
     , gues : List Char
     , word : Word
     }
-type Msg = Gues | Key Char | DelChar | Won | Nil
-
-init : () -> (Model, Cmd Msg)
-init _ = (Model [] [] <| Word ['t', 'e', 's', 't', 'i'], Cmd.none)
+type Model = Loading Status | Game Status State
+type Status = ShortInput | NotInWordList | Ok | YouWon | YouLose
+type Msg = Gues | Key Char | DelChar | Won | Lose | ResetStatus | NewGame Word | Nil
+init : Status -> () -> (Model, Cmd Msg)
+init status _ = (Loading status, Random.generate (\ind -> Array.get ind Words.commonWords |> Maybe.withDefault "debug" |> String.toList |> Word |> NewGame) <| Random.int 0 <| Array.length Words.commonWords)
 
 main = Browser.element
-    { init = init
+    { init = init Ok
     , update = update
     , view = view
     , subscriptions = subscriptions
@@ -41,33 +52,63 @@ keyToMsg string = case String.uncons string of
        _ -> Nil
 
 
-validGues l = List.length l == 5
+validGues l = List.member (List.reverse l |> String.fromList |> String.toLower) Words.allWords
+
+delay time msg = Process.sleep time |> Task.perform (\_ -> msg)
+resetStatus = delay 5000 ResetStatus
+
+update msg model = case model of
+    Loading status -> case msg of
+       NewGame w -> (Game status <| State [] [] w, Cmd.none)
+       _ -> Debug.todo "Stagod"
+    Game status state -> case msg of
+        Gues -> if (List.reverse state.gues |> Word) == state.word then update Won <| Game status state
+            else if List.length state.gues < 5 then (Game ShortInput state, resetStatus)
+            else if not <| validGues state.gues then (Game NotInWordList state, resetStatus)
+            else if List.length state.gueses == 5 then update Lose <| Game status state
+            else (Game status { state | gueses = Word (List.reverse state.gues) :: state.gueses, gues = [] }, Cmd.none)
+        Key  k -> (Game status { state | gues = if List.length state.gues < 5 then k :: state.gues else state.gues }, Cmd.none)
+        DelChar -> case state.gues of
+           _ :: t -> (Game status { state | gues = t }, Cmd.none)
+           [] -> (Game status state, Cmd.none)
+        Won -> let newGame = init YouWon ()  in  (newGame |> Tuple.first, Cmd.batch [newGame |> Tuple.second, resetStatus])
+        Lose -> let newGame = init YouLose () in (newGame |> Tuple.first, Cmd.batch [newGame |> Tuple.second, resetStatus])
+        ResetStatus -> (Game Ok state, Cmd.none)
+        Nil -> (model, Cmd.none)
+        _ -> Debug.todo "Stagod"
 
 
-update msg model =
-    (case msg of
-        Gues -> if (List.reverse model.gues |> Word) == model.word
-            then update Won model |> Tuple.first
-            else if validGues model.gues
-            then { model | gueses = Word (List.reverse model.gues) :: model.gueses, gues = [] }
-            else model
-        Key  k -> { model | gues = if List.length model.gues < 5 then k :: model.gues else model.gues }
-        DelChar -> case model.gues of
-           h :: t -> { model | gues = t }
-           [] -> model
-        Won -> init () |> Tuple.first 
-        Nil -> model
-    , Cmd.none)
+zip : List a -> List b -> List (a, b)
+zip = List.map2 Tuple.pair
 
-toRow (Word final) (Word gues) = List.map2
-    (\y -> \x ->
-        if x==y then cell "green" (Just x)
-        else if List.member x final then cell "yellow" (Just x)
-        else cell "gray" (Just x)
-    ) final gues |> row
+type Color = X | Y | G
+toHtmlColor clr = case clr of 
+    X -> "gray"
+    Y -> "yellow"
+    G -> "green"
+
+toColors ans gues = 
+    let
+        greens = List.map2 (==) ans gues
+        newMask ch list mask = zip mask list |> foldr
+            (\(masked, cur) -> \(changed, acc) -> 
+                if not changed && not masked && cur == ch 
+                then (True, True :: acc) 
+                else (changed, masked :: acc)) (False, []
+            )
+        toColor x y mask = if x==y then G
+            else if newMask x ans mask |> Tuple.first then Y
+            else X
+    in
+        zip gues ans |> foldl (\(x, y) -> \(mask, sol) -> 
+            (newMask x ans mask |> Tuple.second, toColor x y mask :: sol)
+        ) (greens, []) |> Tuple.second |> List.reverse
+
+toRow (Word ans) (Word gues) =
+    List.map2 (\col -> cell (toHtmlColor col) << Just) (toColors ans gues) gues |> row
 
 cell color letter = div
-    [ style "color" color -- could be background-color (as in original)
+    [ style "background-color" color -- could be background-color (as in original)
 
     , style "display" "inline-flex"
     , style "justify-content" "center"
@@ -96,16 +137,16 @@ row cells = div
     , style "height" "100%"
     ] cells
 
-rowFromGues =  List.reverse >> List.map Just >> fill 5 Nothing >> List.map (cell "gray") >> row
+bgColor = "#202020"
+rowFromGues =  List.reverse >> List.map Just >> fill 5 Nothing >> List.map (cell bgColor) >> row
+
 rowDefault = rowFromGues []
 
 fill n empty l = l ++ List.repeat (n - List.length l) empty
 
-table model = fill 6 rowDefault <| List.reverse <| rowFromGues model.gues :: List.map (toRow model.word) model.gueses
+tableRows state = fill 6 rowDefault <| List.reverse <| rowFromGues state.gues :: List.map (toRow state.word) state.gueses
 
-
-view model =
-  div
+table state = div
     [ style "display" "flex"
     , style "flex-direction" "column"
     , style "flex-wrap" "nowrap"
@@ -113,5 +154,48 @@ view model =
 
     , style "width" "350px"
     , style "height" "420px"
-    ] (table model ++ [ button [ onClick DelChar] [ text "Delete Char"] ])
+    ] (tableRows state ++ [ button [ onClick DelChar] [ text "Delete Char"] ])
 
+view model = div
+    [ style "display" "flex"
+    , style "justify-content" "center"
+    , style "flex-direction" "row"
+
+    , style "width" "100%"
+    , style "height" "100%"
+    , style "position" "fixed"
+
+    , style "background-color" bgColor 
+    ]
+    [ div 
+        [ style "display" "flex"
+        , style "justify-content" "center"
+        , style "flex-direction" "column"
+        ]
+        [ statusDiv model
+        , case model of
+            Loading _ -> text "Loading...."
+            Game _ state -> table state
+        , div -- eventualy, will be used for keyboard
+            [ style "height" "300px" ]
+            []
+        ]
+    ]
+
+toString (Word l) = String.fromList l
+
+statusDiv model =
+    let status = case model of
+            Loading stat -> stat
+            Game stat _ -> stat
+        ans = case model of 
+            Loading _ -> "DEBUG"
+            Game _ { word } -> toString word
+    in case status of
+        Ok -> div [ style "height" "100px" ] []
+        ShortInput -> div [ style "height" "100px", style "color" "red" ] [ text "Prekratka ti je rec!"] 
+        NotInWordList -> div [ style "height" "100px", style "color" "red" ] [ text "Ne znam za takvu rec!"] 
+        YouWon -> div [ style "height" "100px", style "color" "green" ] [ text "Bravo! Pobedio si :D" ]
+        YouLose -> div [ style "height" "100px", style "color" "green" ] [ text ("Rec je bila " ++ ans ++ " Probaj ponovo :)") ]
+
+-- rec podne  i pomoc
